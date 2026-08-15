9ee649f5f8e9f133dec07a2170383251e43fed2f
Ivan Radev <107320114+R-zine@users.noreply.github.com>
2026-08-15 19:57:11 +0300

    testing completed

--- cat_os/tests/basic_boot.rs
+++ cat_os/tests/basic_boot.rs
@@ -1,32 +1,27 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(cat_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
+#![test_runner(cat_os::test_runner)]
 
 use core::panic::PanicInfo;
 use cat_os::println;
 
-#[unsafe(no_mangle)]
+#[unsafe(no_mangle)] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-/// This function is called on panic.
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
 
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     cat_os::test_panic_handler(info)
+}
+
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
